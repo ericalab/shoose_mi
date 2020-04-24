f437f5d3d4cecd10190f0351f854373e59c48ff8
ericalab <erika.yyma@gmail.com>
2020-04-24 17:36:18 +0900

    seed追加

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)      not null
+#  name        :string(255)      not null
+#  price       :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_products_on_name  (name)
+#
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
