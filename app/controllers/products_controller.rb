63160616b0fa5a169980a08f49930cc265edafed
ericalab <erika.yyma@gmail.com>
2020-05-02 17:22:31 +0900

    Gem'hashid-rails'を追加し、idをハッシュ化

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,15 @@
 class ProductsController < ApplicationController
-    def index
-        @products = Product.page(params[:page])
-    end
+  def index
+      @products = Product.page(params[:page])
+  end
 
-    def new
-        @product = Product.new
-    end
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+      @product = Product.new
+  end
 
   def create
     @product = Product.new(product_params)
@@ -18,7 +22,7 @@ class ProductsController < ApplicationController
   end
 
   private
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :image)
-  end
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :image)
+    end
 end
