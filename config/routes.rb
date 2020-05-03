d8e9da4eab8188eaea2d57fe4f79119843a71768
ericalab <erika.yyma@gmail.com>
2020-05-03 19:55:45 +0900

    routesにscopeを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
-  resources :products, only: [:new, :show, :create]
+  resources :products, only: [:new, :show, :create] do
+    scope module: :products do
+      resouces :add_to_baskets, only: [:create]
+      resouces :delete_in_baskets, only: [:create]
+    end
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
