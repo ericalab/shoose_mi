8c93859636cd2f0ead93e9b6ec0c34f53d067109
ericalab <erika.yyma@gmail.com>
2020-05-03 19:47:31 +0900

    User/Product/Basket/BasketProductモデルにリレーションを定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,6 +17,8 @@
 class Product < ApplicationRecord
     include Hashid::Rails
     mount_uploader :image, ImageUploader
+    has_many :basket_products, dependent: :destroy
+
     validates :name, presence: true
     validates :description, presence: true
     validates :price, presence: true
