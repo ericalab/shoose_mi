18f8d1f134693d34a59e2190691e9bac603fad9c
ericalab <erika.yyma@gmail.com>
2020-05-22 21:52:19 +0900

    リファクタリング　トランザクション処理追加

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,13 +4,7 @@ class ChargesController < ApplicationController
         token = params[:stripeToken]
         product_ids = params[:product_ids].map(&:to_i)
 
-        products = current_user.basket.products.where(id: product_ids)
-        total = products.sum(:price)
-
-        basket_products = current_user.basket.basket_products.where(product_id: product_ids)
-        basket_products.each(&:destroy!)
-
-        Charge.create!(total, token)
+        current_user.checkout!(token, product_ids: product_ids)
         redirect_to root_path, notice: '決済に成功しました'
     end
 end
